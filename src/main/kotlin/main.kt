effc312d31e78b2336d69c13f4843f497e40ca3f
lapmmos <mielrb@yandex.ru>
2022-04-19 16:52:14 +0300

    New

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,12 @@
-class main {
+fun main() {
+    print("Введите сумму которую хотите перевести (руб.): ")
+    val read = readln().toDouble()
+    val amount:Int = (read*100).toInt()
+    val coefficient: Double =0.0075
+    val minCom:Int= 35*100
+    val totalCom =(if((amount*coefficient)<=minCom) minCom else amount*coefficient).toInt()
+    val kop:Int = (totalCom%100).toInt()
+    val rub:Int =(totalCom/100).toInt()
+
+    println("Сумма комиссии составляет $rub руб. $kop  коп.")
 }
